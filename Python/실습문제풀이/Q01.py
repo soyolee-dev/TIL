3200621446e1a132af2f324ae5ef3fb2f238ebc1
soyolee-dev <soyolee.dev@gmail.com>
2022-07-13 15:26:36 +0900

    220713 파이썬 실습문제풀이

--- Python/실습문제풀이/Q01.py
+++ Python/실습문제풀이/Q01.py
@@ -1,5 +1,7 @@
 # 주어진 수 n이 3의 배수이면서 짝수인 경우 ‘참’을 거짓인 경우 ‘거짓'을 출력하시오.
 
+# 1)
+
 n = input()
 n = int(n)
 
@@ -8,5 +10,15 @@ if n % 3 == 0:
         print('참')
     else: 
         print('거짓')
+else:
+    print('거짓')
+
+
+# 2)
+
+n = int(input())
+
+if n % 2 == 0 and n % 3 == 0:
+    print('참')
 else:
     print('거짓')
--- Python/실습문제풀이/Q04.py
+++ Python/실습문제풀이/Q04.py
@@ -10,3 +10,15 @@ while a <= n:
     result *= a
     a += 1
 print(result)
+
+
+# 2) for문
+
+a = 1
+n = int(input())
+result = 1
+
+for i in range(1, n+1):
+    result *= i
+    i += 1
+print(result)
